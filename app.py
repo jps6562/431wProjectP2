b3f299e0d10803e398482b8117a8dd19ec89d8e2
jps6562 <jselnekovicjr@gmail.com>
2022-03-22 03:02:45 -0400

    USERS TABLE populated

--- app.py
+++ app.py
@@ -6,12 +6,18 @@ host = 'http://127.0.0.1:5000/'
 
 @app.route('/')
 def mainPage():  # put application's code here
-    return 'Hello World!'
+
+    return popData()
+
 
 def popData():
     db = sql.connect('Phase2.db')
     db.execute('CREATE TABLE IF NOT EXISTS Users("email" TEXT PRIMARY KEY, "password" TEXT);')
-
+    inData = pd.read_csv('NittanyMarketDataset-Final/Users.csv')
+    inData.to_sql("Users", db, if_exists='append',index=False)
+    cursor = db.execute('SELECT * FROM Users e;')
+    for i in cursor.fetchall():
+        print(i)
     db.execute('CREATE TABLE IF NOT EXISTS Buyers("email" TEXT PRIMARY KEY, "first_name" TEXT, "last_name" TEXT, "gender" TEXT,'
                ' "age" INTEGER, home_address_id TEXT, billing_address_id TEXT );')
 
@@ -46,7 +52,5 @@ def popData():
                '"Date" TEXT, "Rating" INTEGER, "Rating_Desc" TEXT);')
 
 
-
-
-    if __name__ == '__main__':
+if __name__ == '__main__':
     app.run()
